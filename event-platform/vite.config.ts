4a9b90ace23e31f97840af31f2f47753420327cb
Matheus Chiodi <matheuschiodi20@gmail.com>
2022-06-23 15:13:13 -0300

    Aula 1 • O início da especialização em ReactJS

---
+++ event-platform/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // plugins usados
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
+
+// arquivo de config
